679c7a06339af43fbe280c4ff5761ee766f45c42
ryabinna <ryabinna1986@mail.ru>
2020-12-23 16:13:12 +0300

    radio

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,67 @@
 export const radioPlayerInit = () => {
-console.log('Radio Init');
+     
+
+    const radio  = document.querySelector('.radio'); 
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioCoverImg  = document.querySelector('.radio-cover__img');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioBtns = document.querySelectorAll('.radio-btn');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+    
+    const stopAndPlay = () => {    //остановка и пуск радио
+        if (audio.paused) {
+            audio.play();
+            radio.classList.add('play');
+            radioStop.classList.remove('fa-stop');
+            radioStop.classList.add('fa-play');
+        } else {
+            audio.pause();
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+         }
+    }
+
+
+    const pauseAndPlayOfCurrentStantion = () => { //функционал: если нажать на играющую станцию, она становится на паузу (не всегда срабатывает)
+        radioBtns.forEach(item  => {
+            if (item.checked) {
+                item.addEventListener('click', () => {
+                    stopAndPlay();
+                })
+            }
+         })
+    }
+     
+
+    radioNavigation.addEventListener('change', (event) => {
+        pauseAndPlayOfCurrentStantion();
+        const target = event.target;
+        audio.src = target.dataset.radioStantion;
+        radioStop.disabled = false;
+        stopAndPlay();
+        const parrent = target.closest('.radio-item');
+        radioItem.forEach(item => {
+            item.classList.remove('select');
+        })
+        parrent.classList.add('select');
+        const imgUrl = parrent.querySelector('.radio-img').src;
+        radioCoverImg.src = imgUrl;
+        const nameStantion = parrent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = nameStantion;
+    })
+
+    radioStop.addEventListener('click', () => {
+       stopAndPlay();
+    })
+
+
+
+
 }
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -7,7 +7,19 @@ export const videoPlayerInit = () => {
     const videoProgress = document.querySelector('.video-progress');
     const videoTimeTotal = document.querySelector('.video-time__total');
 
+<<<<<<< HEAD
+
+    const volumeButtonLow = document.querySelector('.volume-button__low');
+    const volumeRange = document.querySelector('.volume-range');
+    const volumeButtonUp = document.querySelector('.volume-button__up');
+    const volumeButtonOff = document.querySelector('.volume-button__off');
+    const fullScrinButton = document.querySelector('.fullScrin-button');
+
+   videoPlayer.volume = volumeRange.value; // установим громкость при первоначальном включении на уровень ползунка, то есть на середину
+
+=======
   
+>>>>>>> d43e33fd7b36165e759d4b5eda128f87077405c3
     const toggleVideo = () => {
         if (videoPlayer.paused) {
             videoPlayer.play();
@@ -47,10 +59,53 @@ export const videoPlayerInit = () => {
         videoProgress.value = (currentTime / duration) * 100;
     })
 
+<<<<<<< HEAD
+    videoProgress.addEventListener('input', () => {
+        videoPlayer.currentTime = (videoPlayer.duration * videoProgress.value) / 100;
+    })
+
+    fullScrinButton.addEventListener('click', () => {  //выводим изображение в полноэкранный режим
+        videoPlayer.requestFullscreen();
+    })
+
+    volumeRange.addEventListener('input', () => { // регулировка громкости рэнджем. В отличии от урока сразу рендж сделела со шкалой от 0 до 1 с шагом 0,2
+        const value = volumeRange.value;
+        videoPlayer.volume = value ;
+    })
+
+    volumeButtonUp.addEventListener('click', () => { //увеличение громкости по нажатию на иконку
+        const currentVolume =  videoPlayer.volume;
+        if (currentVolume <= 0.9) {
+            videoPlayer.volume = currentVolume + 0.1;
+            volumeRange.value = videoPlayer.volume;
+        }
+    })
+
+    volumeButtonLow.addEventListener('click', () => { //уменьшение громкости по нажатию на иконку
+        const currentVolume =  videoPlayer.volume;
+        if (currentVolume >= 0.1) {
+            videoPlayer.volume = currentVolume - 0.1;
+            volumeRange.value = videoPlayer.volume;
+        }
+    })
+
+    volumeButtonOff.addEventListener('click', ()=> { //выключение звука. При выключении иконка становится красной
+       if (videoPlayer.volume == 0) {
+            videoPlayer.volume = volumeRange.value;
+            volumeButtonOff.classList.remove('mute-red');
+            return;
+        }
+        if (videoPlayer.volume > 0) {
+            videoPlayer.volume = 0;
+            volumeButtonOff.classList.add('mute-red');
+        }
+    })
+=======
     videoProgress.addEventListener('change', () => {
         videoPlayer.currentTime = (videoPlayer.duration * videoProgress.value) / 100;
     })
 
 
+>>>>>>> d43e33fd7b36165e759d4b5eda128f87077405c3
 
 }
